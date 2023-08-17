27074a455ad41a4f3cb30782587f0fba435b0e0f
sourcx <25772871+sourcx@users.noreply.github.com>
2023-08-17 11:03:37 +0200

    Try out SameSite=None for cookies.

--- api/login.ts
+++ api/login.ts
@@ -1,6 +1,6 @@
 import strava from 'strava-v3';
 import { serialize } from 'cookie';
-import axios from 'axios';
+// import axios from 'axios';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
@@ -12,25 +12,41 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 
       // Set cookies
       res.setHeader('Set-Cookie', [
-        serialize('access_token', payload.access_token, { expires: new Date(payload.expires_at * 1000), path: '/' }),
-        serialize('refresh_token', payload.refresh_token, { expires: new Date('9999'), path: '/' }),
+        serialize('access_token', payload.access_token, {
+          expires: new Date(payload.expires_at * 1000),
+          path: '/',
+          samesite: 'none',
+        }),
+        serialize('refresh_token', payload.refresh_token, {
+          expires: new Date('9999'),
+          path: '/',
+          samesite: 'none',
+        }),
       ]);
 
-      if (!req.query.refresh) {
-        await axios.post(`https://maker.ifttt.com/trigger/stravanity/with/key/${process.env.IFTTT_API_KEY}`, {
-          value1: payload.athlete.firstname,
-          value2: payload.athlete.lastname,
-          value3: JSON.stringify(payload.athlete),
-        });
-      }
+      // if (!req.query.refresh) {
+      //   await axios.post(`https://maker.ifttt.com/trigger/stravanity/with/key/${process.env.IFTTT_API_KEY}`, {
+      //     value1: payload.athlete.firstname,
+      //     value2: payload.athlete.lastname,
+      //     value3: JSON.stringify(payload.athlete),
+      //   });
+      // }
 
       res.redirect(302, '/');
       return;
     } catch (e) {
       // Clear cookies
       res.setHeader('Set-Cookie', [
-        serialize('access_token', '_', { expires: new Date(0), path: '/' }),
-        serialize('refresh_token', '_', { expires: new Date(0), path: '/' }),
+        serialize('access_token', '_', {
+          expires: new Date(0),
+          path: '/',
+          samesite: 'none',
+        }),
+        serialize('refresh_token', '_', {
+          expires: new Date(0),
+          path: '/',
+          samesite: 'none',
+        }),
       ]);
       res.redirect(302, '/');
       return;
